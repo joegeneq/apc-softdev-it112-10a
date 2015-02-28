7872c0991ff35068a5701543b39e3bddd7338d89
Randolph Bermejo <iamraaandy@gmail.com>
2015-02-28 09:23:26 +0800

    E. Changing database name of ourspace/config/db.php

--- Practice/midterm_test/Bermejo_Randolph_John_midterms/ourspace/config/db.php
+++ Practice/midterm_test/Bermejo_Randolph_John_midterms/ourspace/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=it111_exercise1',
+    'dsn' => 'mysql:host=localhost;dbname=softdev_Bermejo_Randolph_John_ourspace',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
