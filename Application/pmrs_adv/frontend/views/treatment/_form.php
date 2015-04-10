be6dbf707520aeb14576c29f8895d3c518d7442b
Franz Guzman <franzching0802@gmailcom>
2015-04-11 05:37:39 +0800

    Change textInput into dropDownList in the table treatment

--- Application/pmrs_adv/frontend/views/treatment/_form.php
+++ Application/pmrs_adv/frontend/views/treatment/_form.php
@@ -14,9 +14,14 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'treatment_name')->textInput(['maxlength' => 100]) ?>
 
-    <?= $form->field($model, 'category')->textInput(['maxlength' => 100]) ?>
+    <!--<?= $form->field($model, 'category')->textInput(['maxlength' => 100]) ?> --!>
+    
+     <?php echo $form->field($model, 'category')->dropDownList(['Neoadjuvant' => 'Neoadjuvant', 'Adjuvant' => 'Adjuvant']); ?>
+    
 
-    <?= $form->field($model, 'sub_category')->textInput(['maxlength' => 100]) ?>
+    <!--<?= $form->field($model, 'sub_category')->textInput(['maxlength' => 100]) ?>--!>
+    <?php echo $form->field($model, 'sub_category')->dropDownList(['Chemotherapy' => 'Chemotherapy', 'Hormonal' => 'Hormonal']); ?>
+    
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
