81d98f7f5da48834e6f44d5d77b2b61d3bf2641c
Franz Guzman <franzching0802@gmailcom>
2015-04-11 05:41:09 +0800

    add a Operation function to direct to the operation

--- Application/pmrs_adv/frontend/controllers/SiteController.php
+++ Application/pmrs_adv/frontend/controllers/SiteController.php
@@ -115,6 +115,11 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+    
+    public function actionOperations()
+    {
+        return $this->render('operations');
+    }
 
     public function actionSignup()
     {
