7a1458f4d8bbcb2f4d81c4aab60ec62e8ab5710a
Franz Guzman <franzching0802@gmailcom>
2015-04-11 05:36:54 +0800

    Change textInput Into dropDownList in the table breastpanel

--- Application/pmrs_adv/frontend/views/breastpanel/_form.php
+++ Application/pmrs_adv/frontend/views/breastpanel/_form.php
@@ -12,7 +12,15 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'breast_panel_name')->textInput(['maxlength' => 100]) ?>
+    <!--<?= $form->field($model, 'breast_panel_name')->textInput(['maxlength' => 100]) ?> --!>
+     <?php echo $form->field($model, 'breast_panel_name')->dropDownList(['ER positive-PR positive-Her 2 positive ' => 'ER positive-PR positive-Her 2 positive',
+        'ER negative-PR negative-Her 2 negative' => 'ER negative-PR negative-Her 2 negative',
+        'ER Positive-PR negative-Her 2 positive'=>'ER Positive-PR negative-Her 2 positive',
+        ' ER positive-PR  negative-Her  2  negative'=>'ER positive-PR  negative-Her  2  negative',
+        ' ER negative-PR  positive-Her  2  positive'=>'ER negative-PR  positive-Her  2  positive',
+        'ER negative-PR  negative-Her  2  positive'=>'ER negative-PR  negative-Her  2  positive',
+        'ER positive-PR  positive-Her  2  negative'=>'ER positive-PR  positive-Her  2  negative'
+        ]); ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
