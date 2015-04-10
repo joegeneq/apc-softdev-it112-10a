03eb91b72d93a384d8abfbaff18baebc7892efb5
Franz Guzman <franzching0802@gmailcom>
2015-04-11 05:38:25 +0800

    Change the textInput into dropDownList in the table diagnosis

--- Application/pmrs_adv/frontend/views/diagnosis/_form.php
+++ Application/pmrs_adv/frontend/views/diagnosis/_form.php
@@ -12,7 +12,17 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'diagnosis_name')->textInput(['maxlength' => 100]) ?>
+   <!-- <?= $form->field($model, 'diagnosis_name')->textInput(['maxlength' => 100]) ?> --!>
+   
+   <?php echo $form->field($model, 'diagnosis_name')->dropDownList(['Invasive Ductal CA ' => 'Invasive Ductal CA ',
+        'Invasive Lobular CA' => 'Invasive Lobular CA',
+        'Ductal Carcinoma-in-situ  (DCIS) without Comedo'=>'Ductal Carcinoma-in-situ  (DCIS) without Comedo',
+        'Ductal Carcinoma-in-situ  (DCIS) with Comedo'=>'Ductal Carcinoma-in-situ  (DCIS) with Comedo',
+        'Lobular Carcinoma-in-situ (LCIS)'=>'Lobular Carcinoma-in-situ (LCIS)',
+        'Mucinous CA'=>'Mucinous CA',
+        'Papillary CA'=>'Papillary CA',
+        'Tubular CA'=>'Tubular CA',
+       'Medullary CA'=>'Medullary CA']); ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
