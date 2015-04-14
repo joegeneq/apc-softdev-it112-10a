2a35bf78bfff283283dfbeb1d0d0965784d76351
Randolph Bermejo <iamraaandy@gmail.com>
2015-04-15 05:44:10 +0800

    Update the MedicalRecordController

--- Application/pmrs_adv/frontend/controllers/MedicalRecordController.php
+++ Application/pmrs_adv/frontend/controllers/MedicalRecordController.php
@@ -44,21 +44,12 @@ class MedicalRecordController extends Controller
     /**
      * Displays a single MedicalRecord model.
      * @param integer $id
-     * @param integer $patient_id
-     * @param integer $stages_id
-     * @param integer $diagnosis_id
-     * @param integer $breast_panel_id
-     * @param integer $ki_67_id
-     * @param integer $surgery_id
-     * @param integer $user_id
-     * @param integer $histolgic_grading_id
-     * @param integer $treatment_id
      * @return mixed
      */
-    public function actionView($id, $patient_id, $stages_id, $diagnosis_id, $breast_panel_id, $ki_67_id, $surgery_id, $user_id, $histolgic_grading_id, $treatment_id)
+    public function actionView($id)
     {
         return $this->render('view', [
-            'model' => $this->findModel($id, $patient_id, $stages_id, $diagnosis_id, $breast_panel_id, $ki_67_id, $surgery_id, $user_id, $histolgic_grading_id, $treatment_id),
+            'model' => $this->findModel($id),
         ]);
     }
 
@@ -72,7 +63,7 @@ class MedicalRecordController extends Controller
         $model = new MedicalRecord();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id, 'patient_id' => $model->patient_id, 'stages_id' => $model->stages_id, 'diagnosis_id' => $model->diagnosis_id, 'breast_panel_id' => $model->breast_panel_id, 'ki_67_id' => $model->ki_67_id, 'surgery_id' => $model->surgery_id, 'user_id' => $model->user_id, 'histolgic_grading_id' => $model->histolgic_grading_id, 'treatment_id' => $model->treatment_id]);
+            return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('create', [
                 'model' => $model,
@@ -84,23 +75,14 @@ class MedicalRecordController extends Controller
      * Updates an existing MedicalRecord model.
      * If update is successful, the browser will be redirected to the 'view' page.
      * @param integer $id
-     * @param integer $patient_id
-     * @param integer $stages_id
-     * @param integer $diagnosis_id
-     * @param integer $breast_panel_id
-     * @param integer $ki_67_id
-     * @param integer $surgery_id
-     * @param integer $user_id
-     * @param integer $histolgic_grading_id
-     * @param integer $treatment_id
      * @return mixed
      */
-    public function actionUpdate($id, $patient_id, $stages_id, $diagnosis_id, $breast_panel_id, $ki_67_id, $surgery_id, $user_id, $histolgic_grading_id, $treatment_id)
+    public function actionUpdate($id)
     {
-        $model = $this->findModel($id, $patient_id, $stages_id, $diagnosis_id, $breast_panel_id, $ki_67_id, $surgery_id, $user_id, $histolgic_grading_id, $treatment_id);
+        $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id, 'patient_id' => $model->patient_id, 'stages_id' => $model->stages_id, 'diagnosis_id' => $model->diagnosis_id, 'breast_panel_id' => $model->breast_panel_id, 'ki_67_id' => $model->ki_67_id, 'surgery_id' => $model->surgery_id, 'user_id' => $model->user_id, 'histolgic_grading_id' => $model->histolgic_grading_id, 'treatment_id' => $model->treatment_id]);
+            return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('update', [
                 'model' => $model,
@@ -112,20 +94,11 @@ class MedicalRecordController extends Controller
      * Deletes an existing MedicalRecord model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
      * @param integer $id
-     * @param integer $patient_id
-     * @param integer $stages_id
-     * @param integer $diagnosis_id
-     * @param integer $breast_panel_id
-     * @param integer $ki_67_id
-     * @param integer $surgery_id
-     * @param integer $user_id
-     * @param integer $histolgic_grading_id
-     * @param integer $treatment_id
      * @return mixed
      */
-    public function actionDelete($id, $patient_id, $stages_id, $diagnosis_id, $breast_panel_id, $ki_67_id, $surgery_id, $user_id, $histolgic_grading_id, $treatment_id)
+    public function actionDelete($id)
     {
-        $this->findModel($id, $patient_id, $stages_id, $diagnosis_id, $breast_panel_id, $ki_67_id, $surgery_id, $user_id, $histolgic_grading_id, $treatment_id)->delete();
+        $this->findModel($id)->delete();
 
         return $this->redirect(['index']);
     }
@@ -134,21 +107,12 @@ class MedicalRecordController extends Controller
      * Finds the MedicalRecord model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
      * @param integer $id
-     * @param integer $patient_id
-     * @param integer $stages_id
-     * @param integer $diagnosis_id
-     * @param integer $breast_panel_id
-     * @param integer $ki_67_id
-     * @param integer $surgery_id
-     * @param integer $user_id
-     * @param integer $histolgic_grading_id
-     * @param integer $treatment_id
      * @return MedicalRecord the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
-    protected function findModel($id, $patient_id, $stages_id, $diagnosis_id, $breast_panel_id, $ki_67_id, $surgery_id, $user_id, $histolgic_grading_id, $treatment_id)
+    protected function findModel($id)
     {
-        if (($model = MedicalRecord::findOne(['id' => $id, 'patient_id' => $patient_id, 'stages_id' => $stages_id, 'diagnosis_id' => $diagnosis_id, 'breast_panel_id' => $breast_panel_id, 'ki_67_id' => $ki_67_id, 'surgery_id' => $surgery_id, 'user_id' => $user_id, 'histolgic_grading_id' => $histolgic_grading_id, 'treatment_id' => $treatment_id])) !== null) {
+        if (($model = MedicalRecord::findOne($id)) !== null) {
             return $model;
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');
