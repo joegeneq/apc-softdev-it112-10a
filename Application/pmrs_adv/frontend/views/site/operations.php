db46492c88f471ce981a681e1c1fa5f7d5993ac2
Franz Guzman <franzching0802@gmail.com>
2015-04-15 11:29:03 +0800

    add a calendar tab

--- Application/pmrs_adv/frontend/views/site/operations.php
+++ Application/pmrs_adv/frontend/views/site/operations.php
@@ -7,93 +7,41 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
-
-    <center>
-    <table>
-            <tr>
-                <th>Create</th>
-            </tr>
-            <tr>
-                <td> <p>
-        <?= Html::a('Breast Panel', ['/breastpanel/create'], ['class' => 'btn btn-success']) ?>
-    </p>
-                
-                </td>
-                <td> <p>
-        <?= Html::a('Treatment', ['/treatment/create'], ['class' => 'btn btn-danger']) ?>
-    </p>
-                
-                </td>
-                <td> <p>
-        <?= Html::a('Diagnosis', ['/diagnosis/create'], ['class' => 'btn btn-primary']) ?>
-    </p>
-                
-                </td>
-                <td> <p>
-        <?= Html::a('Surgery', ['/surgery/create'], ['class' => 'btn btn-danger']) ?>
-    </p>
-                
-                </td>
-                <td> <p>
-        <?= Html::a('Stages', ['/stages/create'], ['class' => 'btn btn-success']) ?>
-    </p>
-                
-                </td>
-                <td>
-     <p>
-        <?= Html::a('Ki67', ['/ki67/create'], ['class' => 'btn btn-primary']) ?>
-    </p>
-                
-                </td>
-            </tr>
-            
-
-    </table>
-        <table>
-     <tr>
-                <th>View Data</th>
-            </tr>
-            <tr>
-                <td> <p>
-        <?= Html::a('Breast Panel', ['/breastpanel/index'], ['class' => 'btn btn-success']) ?>
-    </p>
-                
-                </td>
-                <td> <p>
-        <?= Html::a('Treatment', ['/treatment/index'], ['class' => 'btn btn-danger']) ?>
-    </p>
-                
-                </td>
-                <td> <p>
-        <?= Html::a('Diagnosis', ['/diagnosis/index'], ['class' => 'btn btn-primary']) ?>
-    </p>
-                
-                </td>
-                <td> <p>
-        <?= Html::a('Surgery', ['/surgery/index'], ['class' => 'btn btn-danger']) ?>
-    </p>
-                
-                </td>
-                <td> <p>
-        <?= Html::a('Stages', ['/stages/index'], ['class' => 'btn btn-success']) ?>
-    </p>
-                
-                </td>
-                <td>
-     <p>
-        <?= Html::a('Ki67', ['/ki67/index'], ['class' => 'btn btn-primary']) ?>
-    </p>
-                
-                </td>
-            </tr>
-            
-    <hr>
-    <table>
-        
-        
-        
-    </table>
-  </center>
+	<div class="row">
+			<div class="col-lg-2">
+				<p><a href="?r=breastpanel">
+                    <img src="images/icon/brestpanel.gif" height="150px" width="150px">
+                </a></p>
+            </div>
+			<div class="col-lg-2">
+				<p><a href="?r=treatment">
+                    <img src="images/icon/treatment.gif" height="150px" width="150px">
+                </a></p>
+            </div>
+			<div class="col-lg-2">
+				<p><a href="?r=diagnosis">
+                    <img src="images/icon/diagnosis.gif" height="150px" width="150px">
+                </a></p>
+            </div>
+			<div class="col-lg-2">
+				<p><a href="?r=surgery">
+                    <img src="images/icon/surgery.gif" height="150px" width="150px">
+                </a></p>
+            </div>
+			<div class="col-lg-2">
+				<p><a href="?r=stages">
+                    <img src="images/icon/stages.gif" height="150px" width="150px">
+                </a></p>
+            </div>
+			<div class="col-lg-2">
+				<p><a href="?r=ki67">
+                    <img src="images/icon/ki67.gif" height="150px" width="150px">
+                </a></p>
+            </div>
+			
+			
+			</div>
+   
     
     
     
